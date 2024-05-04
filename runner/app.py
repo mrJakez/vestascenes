ce49f08ee9a18ace447dc62f8519177e1e656ad7
Dennis Rochel <dennis.rochel@gmail.com>
2024-05-04 14:34:16 +0200

    change runner url to update

--- runner/app.py
+++ runner/app.py
@@ -6,7 +6,7 @@ print("runner started. Python version: " + sys.version)
 
 while True:
     print("runner executed....")
-    r = requests.get("http://controller:8000/")
+    r = requests.get("http://controller:8000/update")
     #print(r.text)
 
     time.sleep(15)  # pause for 15 seconds
