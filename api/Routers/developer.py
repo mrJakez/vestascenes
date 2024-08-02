95e65e8a7e257a4b7dcd927945e20b5083b9f084
Dennis Rochel <dennis.rochel@gmail.com>
2024-08-02 17:14:19 +0200

    increase version to 5

--- api/Routers/developer.py
+++ api/Routers/developer.py
@@ -66,7 +66,7 @@ async def priorities():
         vboard_initialized = True
 
     return {
-        "version-check": "4",
+        "version-check": 5,
         "enabled": (not ConfigHelper.is_disabled()),
         "git-hash": git_hash,
         "strava-initialized": Scenes.StravaLastActivityScene.StravaLastActivityScene.is_initialized(),
