2417481bb37984f73493584742865d0ced97e446
Dennis Rochel <dennis.rochel@gmail.com>
2024-08-08 15:09:42 +0200

    release me

--- api/Routers/developer.py
+++ api/Routers/developer.py
@@ -67,7 +67,7 @@ async def priorities():
         vboard_initialized = True
 
     return {
-        "version-check": 15,
+        "version-check": 16,
         "enabled": (not ConfigHelper.is_disabled()),
         "git-hash": git_hash,
         "strava-initialized": Scenes.StravaLastActivityScene.StravaLastActivityScene.is_initialized(),
