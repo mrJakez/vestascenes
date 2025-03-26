35325603211c35a336847f6a56a3197db4f0273b
Dennis Rochel <dennis.rochel@gmail.com>
2025-03-26 16:54:15 +0100

    change runner port also to 80

--- runner/main.py
+++ runner/main.py
@@ -27,7 +27,7 @@ logger.info("runner started. Python version: " + sys.version)
 time.sleep(5)  # pause for 5 seconds - to provide time for the controller to boot up
 while True:
     logger.debug("runner executed")
-    r = requests.get("http://api:8000/execute")
+    r = requests.get("http://api:80/execute")
     # print(r.text)
 
     time.sleep(15)  # pause for 15 seconds
